9478564f49c1df20561a49d9fc74c7c5ad61dcd7
BinhLe <pinglee8338@gmail.com>
2023-04-27 15:30:58 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      spacing: {
+        '17' : '4.5rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
